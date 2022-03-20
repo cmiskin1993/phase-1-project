e8e3e713e445b636cc2aaa9235e8509814985ea3
Carrie Miskin <clove40@gmail.com>
2022-03-20 16:32:41 -0400

    created click event

---
+++ README.md
@@ -0,0 +1,13 @@
+# Phase 1 Project Build
+
+### Overview
+This is to detail the project from a user perspective.
+
+### How to install / use
+This shows users how to install and use your repository
+
+### How to contribute
+If this is an open source project, how can others make contributions
+
+
+
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,19 @@
-// This is for javascripts logic 
+const petLink = () => document.getElementById('pet-link');
+
+// Event Handlers
+const renderFindPetPage = (e) => {
+    e.preventDefault();
+
+    alert('Hi it works!')
+}
+
+
+// Event Listeners
+const attachFindPetLinkEvent = () => {
+    petLink().addEventListener('click', renderFindPetPage);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    attachFindPetLinkEvent();
+})
